4d26a014fca58eca84dc817611170e7a40c2a017
KaRp <pavel.karmishin@yandex.ru>
2025-12-28 13:56:50 +0900

    Передаю настройки smtp явно

--- ClassReunion/settings.py
+++ ClassReunion/settings.py
@@ -142,9 +142,12 @@ EMAIL_PORT = 587
 EMAIL_USE_SSL = False  # если порт 465
 EMAIL_USE_TLS = True  # если порт 587
 
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = "pavel.karmishin@yandex.ru"
+# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+EMAIL_HOST_USER = 'pavel.karmishin@yandex.ru'
+EMAIL_HOST_PASSWORD = "zipohrmbtsxulduq"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
 MEDIA_URL = '/media/'
